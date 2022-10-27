c3e483e33f9cdd227c9e7d187cfcc4edf1985ffe
manzonlucas <manzonlucasm@gmail.com>
2022-10-27 15:07:37 -0300

    add Issue #9 completed (login and signup). Fixed previous css problems

--- cra-frontend/src/pages/Signup.jsx
+++ cra-frontend/src/pages/Signup.jsx
@@ -1,7 +1,44 @@
+import Layout from "../components/Layout";
+
 export default function Signup() {
   return (
     <>
-      <p>Signup</p>
+      <Layout>
+        <section className="loginView">
+          <h1>Crear cuenta</h1>
+          <form action="" className="loginForm">
+
+            <div className="formItemDiv">
+              <label htmlFor="name">Nombre</label>
+              <input type="text" id="name" name="name" className="formInput" />
+            </div>
+
+            <div className="formItemDiv">
+              <label htmlFor="lastname" >Apellido</label>
+              <input type="text" id="lastname" name="lastname" className="formInput" />
+            </div>
+
+            <div className="formItemDiv">
+              <label htmlFor="email" >Correo electrónico</label>
+              <input type="email" id="email" name="email" className="formInput" />
+            </div>
+
+            <div className="formItemDiv">
+              <label htmlFor="password">Contraseña</label>
+              <input type="password" id="password" name="password" className="formInput" />
+            </div>
+
+            <div className="formItemDiv">
+              <label htmlFor="passwordConfirm">Confirmar contraseña</label>
+              <input type="password" id="passwordConfirm" name="passwordConfirm" className="formInput" />
+            </div>
+
+            <button>Crear cuenta</button>
+          </form>
+
+          <p>Ya tienes cuenta? <a href="/login">Iniciar sesión</a></p>
+        </section>
+      </Layout>
     </>
   )
 }
