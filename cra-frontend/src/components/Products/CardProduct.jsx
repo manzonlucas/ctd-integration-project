16ef4116a716036e1c01b19ceeb92a90d7751770
EmanuelGiraldi94 <emanuelgiraldi@gmail.com>
2022-12-09 18:42:19 -0300

    Se agrego cambios en las cards de recomendaciones

--- cra-frontend/src/components/Products/CardProduct.jsx
+++ cra-frontend/src/components/Products/CardProduct.jsx
@@ -14,17 +14,18 @@ export default function CardProduct({ product, index }) {
       </div>
       <div>
         <div className='product-info'>
-          <p className='name-info'>{formatText(product.categoria.toUpperCase())} <RankigCardProduct product={product} /></p> 
-          <p className='titulo-info'>{product.titulo}</p>
+          <p className='name-info'>{formatText(product.categoria.toUpperCase())} <RankigCardProduct product={product} /> <p className='numberRanking'>8</p></p> 
+          <p className='titulo-info'>{product.titulo} <p className='textRanking'>Muy Bueno</p></p>
           {/* TO DO ICONO DE UBICACION */}
           <p className='ciudad-info' ><i class="fa-solid fa-location-dot"></i> {product.ciudad}</p>
           <FeaturesCards product={product} />
           <p className='descripcion-info'>{product.descripcion.length > 150 ? `${product.descripcion.substring(0, 150)}...` : product.descripcion}
           </p>
         </div>
-
+        <div className='boton'> 
         {/* TODO CHEQUEAR ESTE LINK */}
         <Link style={{textDecoration: "none"}} to={`/producto/${product.id}`} className='moreInfo'>Ver detalle</Link>
+      </div>
       </div>
 
     </article >
