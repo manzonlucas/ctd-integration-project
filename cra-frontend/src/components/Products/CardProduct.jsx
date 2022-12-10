ade5bed913bba35618c6b591591a60f837f294e0
manzonlucas <manzonlucasm@gmail.com>
2022-12-09 22:50:46 -0300

    fix some console errors in CardProduct.jsx, delete Ranking component and moved this function to CardProduct.jsx. Pending wrong nesting errors in this component

--- cra-frontend/src/components/Products/CardProduct.jsx
+++ cra-frontend/src/components/Products/CardProduct.jsx
@@ -1,12 +1,16 @@
 import { Link } from 'react-router-dom';
 import { formatText } from '../../util';
 import FeaturesCards from "./FeaturesCardProduct";
-import RankigCardProduct from "./RankigCardProduct";
-
 
 export default function CardProduct({ product, index }) {
+
+  const RANKING = '★★★★★☆☆☆☆☆';
+  function getRankingCard(ranking) {
+    return RANKING.slice(5 - ranking, 10 - ranking);
+  }
+
   return (
-    
+
     <article className="card shadow" key={index}>
       {/* REEMPLAZAR INDEX POR ID UNA VEZ CONECTADO A DB */}
       <div>
@@ -18,18 +22,30 @@ export default function CardProduct({ product, index }) {
       </div>
       <div>
         <div className='product-info'>
-          <p className='name-info'>{formatText(product.categoria.toUpperCase())} <RankigCardProduct product={product} /> <p className='numberRanking'>8</p></p> 
+
+          {/* Quitar nestings de <p/> dentro de <p/> */}
+          <p className='name-info'>{formatText(product.categoria.toUpperCase())}
+            <p className="rankingCard">{getRankingCard(4)}</p>
+            <p className='numberRanking'>8</p>
+          </p>
+
           <p className='titulo-info'>{product.titulo} <p className='textRanking'>Muy Bueno</p></p>
-          {/* TO DO ICONO DE UBICACION */}
-          <p className='ciudad-info' ><i class="fa-solid fa-location-dot"></i> {product.ciudad}</p>
+
+          <p className='ciudad-info'>
+            <i className='fa-solid fa-location-dot'></i>
+            {product.ciudad}
+          </p>
+
           <FeaturesCards product={product} />
+
           <p className='descripcion-info'>{product.descripcion.length > 150 ? `${product.descripcion.substring(0, 150)}...` : product.descripcion}
           </p>
+
         </div>
-        <div className='boton'> 
-        {/* TODO CHEQUEAR ESTE LINK */}
-        <Link style={{textDecoration: "none"}} to={`/producto/${product.id}`} className='moreInfo'>Ver detalle</Link>
-      </div>
+
+        <div className='boton'>
+          <Link style={{ textDecoration: "none" }} to={`/producto/${product.id}`} className='moreInfo'>Ver detalle</Link>
+        </div>
       </div>
 
     </article >
