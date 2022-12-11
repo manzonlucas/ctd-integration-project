1e33acf8f0dd52422f548c4e49e8a6cadf4a8122
EmanuelGiraldi94 <emanuelgiraldi@gmail.com>
2022-12-11 15:03:13 -0300

    espacio entre las casillas de crear usuario y modo responsive

--- cra-frontend/src/pages/Signup.jsx
+++ cra-frontend/src/pages/Signup.jsx
@@ -5,40 +5,40 @@ export default function Signup() {
   return (
     <>
       <Layout>
-        <section className="formView">
-          <h1>Crear cuenta</h1>
+        <section className="formView ">
+          <h1 className="m-bottom-50 m-top-70">Crear cuenta</h1>
           <form action="" className="form">
 
-            <div className="formItemDiv">
+            <div className="formItemDiv m-bottom-20">
               <label htmlFor="name">Nombre</label>
               <input type="text" id="name" name="name" className="formInput" />
             </div>
 
-            <div className="formItemDiv">
+            <div className="formItemDiv m-bottom-20">
               <label htmlFor="lastname" >Apellido</label>
               <input type="text" id="lastname" name="lastname" className="formInput" />
             </div>
 
-            <div className="formItemDiv">
+            <div className="formItemDiv m-bottom-20">
               <label htmlFor="email" >Correo electrónico</label>
               <input type="email" id="email" name="email" className="formInput" />
             </div>
 
-            <div className="formItemDiv">
+            <div className="formItemDiv m-bottom-20">
               <label htmlFor="password">Contraseña</label>
               <input type="password" id="password" name="password" className="formInput" />
             </div>
 
-            <div className="formItemDiv">
+            <div className="formItemDiv m-bottom-20">
               <label htmlFor="passwordConfirm">Confirmar contraseña</label>
               <input type="password" id="passwordConfirm" name="passwordConfirm" className="formInput" />
             </div>
 
-            <input type="submit" className="submitButton" value='Crea cuenta' />
+            <input type="submit" className="submitButton m-bottom-20 m-top-20" value='Crea cuenta' />
           </form>
 
-          <p>Ya tienes cuenta?</p>
-          <Link to='/login' >
+          <p className="m-top-20">Ya tienes cuenta?</p>
+          <Link style={{textDecoration: "none"}} to='/login' >
             Iniciar sesión
           </Link>
         </section>
